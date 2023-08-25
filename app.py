49d0c63253092ef424c1b34750ae8322ff33dcc4
Vinh Bùi Đức <vinhbuiduc@192.168.2.7>
2023-08-25 13:50:51 +0700

    sick my duck

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Fuck you", page_icon=":heart:", layout="wide")
+
+
+st.subheader("Hello, I am Cristiano Ronaldo")
+st.title("I am the goat")
+st.write("Siiuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
+st.write("this is why: https://www.youtube.com/watch?v=dQw4w9WgXcQ")
